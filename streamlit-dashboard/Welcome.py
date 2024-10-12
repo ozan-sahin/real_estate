d8f16c2f5899dbfc429c01fbe57f59a6d5bfd8d6
ozan-sahin <50116996+ozan-sahin@users.noreply.github.com>
2024-10-12 18:28:34 +0200

    Update Welcome.py

--- streamlit-dashboard/Welcome.py
+++ streamlit-dashboard/Welcome.py
@@ -1,11 +1,8 @@
 import pandas as pd
-import plotly.express as px
 import matplotlib.pyplot as plt
 import streamlit as st
-#from streamlit_folium import folium_static
-#import folium
-#from sqlalchemy import create_engine
 import datetime, json
+
 plt.style.use('fivethirtyeight')
 
 st.set_page_config(page_title="Real Estate Analytics", page_icon=":house:", layout="wide")
