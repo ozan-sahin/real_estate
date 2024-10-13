8814253fb069adfe52502f44ff3535d23233b9a4
ozan-sahin <50116996+ozan-sahin@users.noreply.github.com>
2024-10-13 10:28:12 +0200

    Update Welcome.py

--- streamlit-dashboard/Welcome.py
+++ streamlit-dashboard/Welcome.py
@@ -1,19 +1,39 @@
 import pandas as pd
 import streamlit as st
 import datetime, json
-
 from streamlit_gsheets import GSheetsConnection
+
+st.set_page_config(page_title="Real Estate Analytics", page_icon=":house:", layout="wide")
 
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
-
-st.set_page_config(page_title="Real Estate Analytics", page_icon=":house:", layout="wide")
 
 st.title(":house: Welcome to Real Estate Analytics")
 
 st.markdown("""---""")
 
 df = conn.read()
+
+ordered_columns = ['image', 'url', 'title', 'city', 'district', 'price', 'area', \
+                   'price_per_m2', 'ref_price', 'sale_ratio', 'return_in_years']
+
+st.dataframe(
+    df[ordered_columns].sort_values(by="PricePerLength"),
+    column_config={
+        "image": st.column_config.ImageColumn('📷Image', width="small"),
+        "url" : st.column_config.LinkColumn('🔗URL'),
+        "price_per_m2" : st.column_config.NumberColumn('💎PricePerArea',format="%0f €/m^2"),
+        "price" : st.column_config.NumberColumn('💶Price',format="%.0f €"),
+        "area" : st.column_config.NumberColumn('📐Area',format="%0f m^2"),
+        "sale_ratio" : st.column_config.ProgressColumn('💰Discount (%)',format="%f",min_value=0,max_value=100),
+        "ref_price" : st.column_config.TextColumn('🏷️ReferencePrice'),
+        "city" : st.column_config.TextColumn('🌍City'),
+        "district" : st.column_config.TextColumn('📌District'),
+        "source" : st.column_config.TextColumn('⚓Source'),
+        "title" : st.column_config.TextColumn('📕Title')
+    },
+    hide_index=True,use_container_width=True
+)
 
 # ---- HIDE STREAMLIT STYLE ----
 hide_st_style = """
