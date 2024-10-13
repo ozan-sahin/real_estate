cc563b15e9cbf5d734c370a5fdbcdf0cee56e82e
ozan-sahin <50116996+ozan-sahin@users.noreply.github.com>
2024-10-13 10:15:25 +0200

    Update Welcome.py

--- streamlit-dashboard/Welcome.py
+++ streamlit-dashboard/Welcome.py
@@ -2,11 +2,18 @@ import pandas as pd
 import streamlit as st
 import datetime, json
 
+from streamlit_gsheets import GSheetsConnection
+
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
 st.set_page_config(page_title="Real Estate Analytics", page_icon=":house:", layout="wide")
 
 st.title(":house: Welcome to Real Estate Analytics")
 
 st.markdown("""---""")
+
+df = conn.read()
 
 # ---- HIDE STREAMLIT STYLE ----
 hide_st_style = """
