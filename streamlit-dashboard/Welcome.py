7ec876ec13b962e08d42a2ff8f760aed3c560bdd
ozan-sahin <50116996+ozan-sahin@users.noreply.github.com>
2025-02-01 12:39:41 +0100

    Update Welcome.py

--- streamlit-dashboard/Welcome.py
+++ streamlit-dashboard/Welcome.py
@@ -72,150 +72,150 @@ column55, column66, column77, column88, column99= st.columns([2, 2, 2, 2, 2])
 with column11:
     low_price, high_price = st.select_slider('Price Range', options=range(0,10000001), value=(0,500000))
 
-with column22:
-    low_area, high_area = st.select_slider('Area', options=range(0,650), value=(60,200))
+# with column22:
+#     low_area, high_area = st.select_slider('Area', options=range(0,650), value=(60,200))
   
-with column33:
-    low_room, high_room = st.select_slider('Room Number', options=range(0,50), value=(3,6))
+# with column33:
+#     low_room, high_room = st.select_slider('Room Number', options=range(0,50), value=(3,6))
 
-with column44:
-    low_return, high_return = st.select_slider('Return in Years', options=range(0,100), value=(0,35))
+# with column44:
+#     low_return, high_return = st.select_slider('Return in Years', options=range(0,100), value=(0,35))
 
-series_city = df.city.value_counts()
-common_cities = series_city[series_city > 15].index.tolist()
+# series_city = df.city.value_counts()
+# common_cities = series_city[series_city > 15].index.tolist()
 
-with column55:
-    locations = st.multiselect("Cities", common_cities,["Düsseldorf"])
-    all_options = st.checkbox("Select all cities", value=False)
+# with column55:
+#     locations = st.multiselect("Cities", common_cities,["Düsseldorf"])
+#     all_options = st.checkbox("Select all cities", value=False)
 
-    if all_options:
-        locations = common_cities
+#     if all_options:
+#         locations = common_cities
 
-with column66:
-    states = st.multiselect("State", df.state.unique().tolist(), "Nordrhein-Westfalen")
+# with column66:
+#     states = st.multiselect("State", df.state.unique().tolist(), "Nordrhein-Westfalen")
 
-with column77:
-    distribution_types = st.multiselect("Type" ,["Buy", "Rent"], ["Rent"])
+# with column77:
+#     distribution_types = st.multiselect("Type" ,["Buy", "Rent"], ["Rent"])
 
-with column88:
-    types = st.multiselect("Estate Type", ["apartment", "house"],["apartment"])
+# with column88:
+#     types = st.multiselect("Estate Type", ["apartment", "house"],["apartment"])
 
-with column99:
+# with column99:
 
-    date_options = ["Today", "Last Week", "Last Month", "All Time"]
-    date_to_select = st.selectbox("Date Range", date_options)
+#     date_options = ["Today", "Last Week", "Last Month", "All Time"]
+#     date_to_select = st.selectbox("Date Range", date_options)
 
-    if date_to_select == "Today":
-        dates = [datetime.date.today().strftime('%Y-%m-%d')]
-    elif date_to_select == "Last Week":
-        dates = pd.date_range(end=datetime.date.today(), periods=7).strftime('%Y-%m-%d').tolist()
-    elif date_to_select == "Last Month":
-        dates = pd.date_range(end=datetime.date.today(), periods=30).strftime('%Y-%m-%d').tolist()
-    else:
-        dates = df.creation_date.dt.strftime('%Y-%m-%d').unique().tolist()
+#     if date_to_select == "Today":
+#         dates = [datetime.date.today().strftime('%Y-%m-%d')]
+#     elif date_to_select == "Last Week":
+#         dates = pd.date_range(end=datetime.date.today(), periods=7).strftime('%Y-%m-%d').tolist()
+#     elif date_to_select == "Last Month":
+#         dates = pd.date_range(end=datetime.date.today(), periods=30).strftime('%Y-%m-%d').tolist()
+#     else:
+#         dates = df.creation_date.dt.strftime('%Y-%m-%d').unique().tolist()
 
-    # dates = st.multiselect("Creation Date", df.creation_date.dt.strftime('%Y-%m-%d').unique().tolist(), date_to_select)
-    # all_options = st.checkbox("Select all dates", value=False)
+#     # dates = st.multiselect("Creation Date", df.creation_date.dt.strftime('%Y-%m-%d').unique().tolist(), date_to_select)
+#     # all_options = st.checkbox("Select all dates", value=False)
 
-    # if all_options:
-    #     dates = df.creation_date.dt.strftime('%Y-%m-%d').unique().tolist()
+#     # if all_options:
+#     #     dates = df.creation_date.dt.strftime('%Y-%m-%d').unique().tolist()
       
-#queried dataframe
-df_query = df.query("price >= @low_price and price <= @high_price") \
-            .query("area >= @low_area and area <= @high_area") \
-            .query("city in @locations") \
-            .query("estate_type in @types") \
-            .query("state in @states") \
-            .query("distribution_type in @distribution_types") \
-            #.query("creation_date.dt.strftime('%Y-%m-%d') in @dates") \
-            #.query("room >= @low_room and room <= @high_room") \
-            #.query("return_in_years >= @low_return and return_in_years <= @high_return")
+# #queried dataframe
+# df_query = df.query("price >= @low_price and price <= @high_price") \
+#             .query("area >= @low_area and area <= @high_area") \
+#             .query("city in @locations") \
+#             .query("estate_type in @types") \
+#             .query("state in @states") \
+#             .query("distribution_type in @distribution_types") \
+#             #.query("creation_date.dt.strftime('%Y-%m-%d') in @dates") \
+#             #.query("room >= @low_room and room <= @high_room") \
+#             #.query("return_in_years >= @low_return and return_in_years <= @high_return")
 
-ordered_columns = ['image', 'title', 'city', 'district', 'price', 'area', 'room','price_per_m2', \
-                    'ref_price', 'sale_ratio', 'return_in_years', 'source', 'creation_date', 'url', "makler"]
+# ordered_columns = ['image', 'title', 'city', 'district', 'price', 'area', 'room','price_per_m2', \
+#                     'ref_price', 'sale_ratio', 'return_in_years', 'source', 'creation_date', 'url', "makler"]
 
-st.dataframe(
-    df_query[ordered_columns].sort_values(by="return_in_years"),
-    column_config={
-        "image": st.column_config.ImageColumn('📷Image', width="small"),
-        "price_per_m2" : st.column_config.NumberColumn('💎PricePerArea',format="%0f €/m²"),
-        "price" : st.column_config.NumberColumn('💶Price',format="%.0f €"),
-        "area" : st.column_config.NumberColumn('📐Area',format="%0f m²"),
-        "room" : st.column_config.NumberColumn('🏨Room'),
-        "sale_ratio" : st.column_config.ProgressColumn('💰Discount (%)',format="%f",min_value=-50,max_value=100),
-        "ref_price" : st.column_config.NumberColumn('🏷️ReferencePrice',format="%0f €/m²"),
-        "return_in_years" : st.column_config.NumberColumn('💰ReturnInYears'),
-        "city" : st.column_config.TextColumn('🌍City'),
-        "district" : st.column_config.TextColumn('📌District'),
-        "source" : st.column_config.TextColumn('⚓Source'),
-        "title" : st.column_config.TextColumn('📕Title'),
-        "creation_date" : st.column_config.DateColumn('📅Creation_Date',format="DD.MM.YYYY"),
-        "url" : st.column_config.LinkColumn('🔗URL'),
-        "makler" : st.column_config.TextColumn('Makler')
-    },
-    hide_index=True,use_container_width=True
-)
+# st.dataframe(
+#     df_query[ordered_columns].sort_values(by="return_in_years"),
+#     column_config={
+#         "image": st.column_config.ImageColumn('📷Image', width="small"),
+#         "price_per_m2" : st.column_config.NumberColumn('💎PricePerArea',format="%0f €/m²"),
+#         "price" : st.column_config.NumberColumn('💶Price',format="%.0f €"),
+#         "area" : st.column_config.NumberColumn('📐Area',format="%0f m²"),
+#         "room" : st.column_config.NumberColumn('🏨Room'),
+#         "sale_ratio" : st.column_config.ProgressColumn('💰Discount (%)',format="%f",min_value=-50,max_value=100),
+#         "ref_price" : st.column_config.NumberColumn('🏷️ReferencePrice',format="%0f €/m²"),
+#         "return_in_years" : st.column_config.NumberColumn('💰ReturnInYears'),
+#         "city" : st.column_config.TextColumn('🌍City'),
+#         "district" : st.column_config.TextColumn('📌District'),
+#         "source" : st.column_config.TextColumn('⚓Source'),
+#         "title" : st.column_config.TextColumn('📕Title'),
+#         "creation_date" : st.column_config.DateColumn('📅Creation_Date',format="DD.MM.YYYY"),
+#         "url" : st.column_config.LinkColumn('🔗URL'),
+#         "makler" : st.column_config.TextColumn('Makler')
+#     },
+#     hide_index=True,use_container_width=True
+# )
 
-# Filter and group data
-most_popular_cities = df.city.value_counts()[df.city.value_counts() > 50].index.tolist()
+# # Filter and group data
+# most_popular_cities = df.city.value_counts()[df.city.value_counts() > 50].index.tolist()
 
-# Streamlit columns
-column1, column2= st.columns([10,6])
+# # Streamlit columns
+# column1, column2= st.columns([10,6])
 
-from random import randrange
-import math
-# Streamlit columns
+# from random import randrange
+# import math
+# # Streamlit columns
 
-def get_lat_lon( address: str) -> tuple:
-    loc = Photon(user_agent="measurements")
-    getLoc = loc.geocode(address)
-    if getLoc:
-        return [float(getLoc.latitude), float(getLoc.longitude)]
-    return [None,None]
+# def get_lat_lon( address: str) -> tuple:
+#     loc = Photon(user_agent="measurements")
+#     getLoc = loc.geocode(address)
+#     if getLoc:
+#         return [float(getLoc.latitude), float(getLoc.longitude)]
+#     return [None,None]
 
-with column1:
-    column1_1, column1_2, column1_3, column1_4 = st.columns([4,2,2,2])
-    with column1_1:
-        # index = st.number_input(label="index", value=13585)
-        link = st.text_input(label="URL to inspect")
-        if link == "":
-            index = randrange(df.shape[0])
-        else:
-            index = df.query("url == @link").index.values[0]
-        st.image(df.iloc[index].image, caption=df.iloc[index].headline)
-        st.markdown(f"[Link to Real Estate]({df.iloc[index].url})")
-        if pd.notna(df.iloc[index].description):
-            with st.expander("See description"):
-                st.write(df.iloc[index].description)
-    with column1_2:
-        st.metric(label="Price", value=f"{df.iloc[index].price:,.0f} €")
-        st.metric(label="Area", value=f"{df.iloc[index].area:,.0f} m²")
-        st.metric(label="City", value=df.iloc[index].city)
-        st.metric(label="District", value=df.iloc[index].district)
-        if pd.notna(df.iloc[index].makler):
-            st.markdown(f"Makler: [{df.iloc[index].makler}]({df.iloc[index].makler_website})")
-    with column1_3:
-        st.metric(label="Price per m²", value=f"{df.iloc[index].price_per_m2:,.0f} €/m²", delta=f"{df.iloc[index].sale_ratio * -1} %", delta_color="inverse")
-        st.metric(label="Reference rent price", value=f"{df.iloc[index].ref_rent_price:,.0f} €/m²")
-        st.metric(label="Return in years", value=df.iloc[index].return_in_years)
-        st.metric(label="Days since creation", value=(datetime.date.today() - df.iloc[index].creation_date.date()).days)
-        st.metric(label="Type", value=df.iloc[index].distribution_type)
-    with column1_4: 
-        try:
-            st.metric(label="Expected annual rent", value=f"{(df.iloc[index].area * df.iloc[index].ref_rent_price * 12):,.0f} €/year")
-            st.metric(label="Expected monthly rent", value=f"{(df.iloc[index].area * df.iloc[index].ref_rent_price):,.0f} €/month")
-            st.metric(label="Yield ", value=f"{round((1 / df.iloc[index].return_in_years) * 100, 2)} %")
-            st.metric(label="Days since last update", value=(datetime.date.today() - df.iloc[index].update_date.date()).days)
-        except ValueError:
-            st.metric(label="", value="")
+# with column1:
+#     column1_1, column1_2, column1_3, column1_4 = st.columns([4,2,2,2])
+#     with column1_1:
+#         # index = st.number_input(label="index", value=13585)
+#         link = st.text_input(label="URL to inspect")
+#         if link == "":
+#             index = randrange(df.shape[0])
+#         else:
+#             index = df.query("url == @link").index.values[0]
+#         st.image(df.iloc[index].image, caption=df.iloc[index].headline)
+#         st.markdown(f"[Link to Real Estate]({df.iloc[index].url})")
+#         if pd.notna(df.iloc[index].description):
+#             with st.expander("See description"):
+#                 st.write(df.iloc[index].description)
+#     with column1_2:
+#         st.metric(label="Price", value=f"{df.iloc[index].price:,.0f} €")
+#         st.metric(label="Area", value=f"{df.iloc[index].area:,.0f} m²")
+#         st.metric(label="City", value=df.iloc[index].city)
+#         st.metric(label="District", value=df.iloc[index].district)
+#         if pd.notna(df.iloc[index].makler):
+#             st.markdown(f"Makler: [{df.iloc[index].makler}]({df.iloc[index].makler_website})")
+#     with column1_3:
+#         st.metric(label="Price per m²", value=f"{df.iloc[index].price_per_m2:,.0f} €/m²", delta=f"{df.iloc[index].sale_ratio * -1} %", delta_color="inverse")
+#         st.metric(label="Reference rent price", value=f"{df.iloc[index].ref_rent_price:,.0f} €/m²")
+#         st.metric(label="Return in years", value=df.iloc[index].return_in_years)
+#         st.metric(label="Days since creation", value=(datetime.date.today() - df.iloc[index].creation_date.date()).days)
+#         st.metric(label="Type", value=df.iloc[index].distribution_type)
+#     with column1_4: 
+#         try:
+#             st.metric(label="Expected annual rent", value=f"{(df.iloc[index].area * df.iloc[index].ref_rent_price * 12):,.0f} €/year")
+#             st.metric(label="Expected monthly rent", value=f"{(df.iloc[index].area * df.iloc[index].ref_rent_price):,.0f} €/month")
+#             st.metric(label="Yield ", value=f"{round((1 / df.iloc[index].return_in_years) * 100, 2)} %")
+#             st.metric(label="Days since last update", value=(datetime.date.today() - df.iloc[index].update_date.date()).days)
+#         except ValueError:
+#             st.metric(label="", value="")
 
-with column2:
-    try:
-        lat, lon = get_lat_lon(df.iloc[index].zip_code + " " + df.iloc[index].city+ " " + df.iloc[index].district)
-        if lat and lon:
-            st.map(pd.DataFrame([{"lat": lat,"lon": lon}]), zoom=6.5, use_container_width=True)
-    except:
-            st.map(pd.DataFrame([{"lat": 51.233,"lon": 6.783}]), zoom=7, use_container_width=True)
+# with column2:
+#     try:
+#         lat, lon = get_lat_lon(df.iloc[index].zip_code + " " + df.iloc[index].city+ " " + df.iloc[index].district)
+#         if lat and lon:
+#             st.map(pd.DataFrame([{"lat": lat,"lon": lon}]), zoom=6.5, use_container_width=True)
+#     except:
+#             st.map(pd.DataFrame([{"lat": 51.233,"lon": 6.783}]), zoom=7, use_container_width=True)
 
 
 # ---- HIDE STREAMLIT STYLE ----
