dab0b6ab5abc490a0bd8ca499d8ae3608a4f311c
ozan-sahin <50116996+ozan-sahin@users.noreply.github.com>
2026-04-26 21:49:53 +0200

    Update 10_Bali.py

--- streamlit-dashboard/pages/10_Bali.py
+++ streamlit-dashboard/pages/10_Bali.py
@@ -134,15 +134,13 @@ with column1:
         st.image(df.iloc[index].image, caption=df.iloc[index].title)
         st.markdown(f"[Link to Real Estate]({df.iloc[index].url})")
     with column1_2:
-        st.metric(label="Price", value=f"{df.iloc[index].price:,.0f} $")
+        st.metric(label="Price", value=f"{df.iloc[index].price:,.0f} €")
         st.metric(label="Area", value=f"{df.iloc[index].area:,.0f} m²")
-        st.metric(label="Bathrooms", value=df.iloc[index].bathrooms)
-        st.metric(label="City", value=df.iloc[index].city)
+        st.metric(label="Rooms", value=df.iloc[index].room)
+        st.metric(label="Neighorhood", value=df.iloc[index].address)
         
     with column1_3:
-        st.metric(label="Price per m²", value=f"{df.iloc[index].price_per_m2:,.0f} $/m²", delta_color="inverse")
-        st.metric(label="Bedrooms", value=df.iloc[index].room)
-        st.metric(label="Address", value=df.iloc[index].address)
+        st.metric(label="Price per m²", value=f"{df.iloc[index].price_per_m2:,.0f} €/m²", delta_color="inverse")
     
 with column2:
     try:
